73a2f6a2ba98b79b579b01e9b843df00a03ae62d
flyingking888 <jeremyfeiwang@outlook.com>
2026-04-22 12:54:15 -0400

    Initial Commit

---
+++ FinalExamWarmupCase/FinalExamWarmupCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalExamWarmupCaseApp.swift
+//  FinalExamWarmupCase
+//
+//  Created by Jeremy Wang on 4/22/26.
+//
+
+import SwiftUI
+
+@main
+struct FinalExamWarmupCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
